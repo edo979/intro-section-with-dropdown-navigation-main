22e45b0fa54b0adf5dc038a04ce8867077f1efe6
edo979 <prezentabilan@gmail.com>
2022-04-20 19:44:40 +0200

    coding mobile dropdown

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const navEl = document.getElementById('nav'),
   openNavBtn = document.getElementById('menu-open'),
-  closeNavBtn = document.getElementById('menu-close')
+  closeNavBtn = document.getElementById('menu-close'),
+  dropdownEls = document.querySelectorAll('.dropdown_title')
 
 openNavBtn.addEventListener('click', (e) => navEl.classList.add('colapsed'))
 closeNavBtn.addEventListener('click', (e) => navEl.classList.remove('colapsed'))
+dropdownEls.forEach((dropdownEl) => {
+  dropdownEl.addEventListener('click', (e) =>
+    dropdownEl.parentElement.classList.toggle('colapsed')
+  )
+})
